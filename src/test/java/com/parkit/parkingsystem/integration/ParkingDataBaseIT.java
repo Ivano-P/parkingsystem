4afbe92f1e8a67d132760ccfcfeb03d5d8c0f39f
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-03-07 17:15:30 +0100

    reformated IT to use ticketDAO to check db instead of creating new queries to check db

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,12 +1,7 @@
 package com.parkit.parkingsystem.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.when;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,10 +12,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 
@@ -36,121 +34,57 @@ public class ParkingDataBaseIT {
     private static InputReaderUtil inputReaderUtil;
 
     @BeforeAll
-    private static void setUp() throws Exception {
-	parkingSpotDAO = new ParkingSpotDAO();
-	parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
-	ticketDAO = new TicketDAO();
-	ticketDAO.dataBaseConfig = dataBaseTestConfig;
-	dataBasePrepareService = new DataBasePrepareService();
+    private static void setUp() throws Exception{
+        parkingSpotDAO = new ParkingSpotDAO();
+        parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
+        ticketDAO = new TicketDAO();
+        ticketDAO.dataBaseConfig = dataBaseTestConfig;
+        dataBasePrepareService = new DataBasePrepareService();
     }
 
     @BeforeEach
     private void setUpPerTest() throws Exception {
-	when(inputReaderUtil.readSelection()).thenReturn(1);
-	when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-	dataBasePrepareService.clearDataBaseEntries();
+        when(inputReaderUtil.readSelection()).thenReturn(1);
+        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+        dataBasePrepareService.clearDataBaseEntries();
     }
 
     @AfterAll
-    private static void tearDown() {
-
+    private static void tearDown(){
+	dataBasePrepareService.clearDataBaseEntries();
     }
 
     @Test
-    public void testParkingACar() throws Exception {
-	// GIVEN
-	ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+    @DisplayName("that a ticket is saved in DB and Parking table is updated with availability")
+    public void testParkingACar(){
+	//GIVEN
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        //WHEN
+        parkingService.processIncomingVehicle();
 
-	// WHEN
-	parkingService.processIncomingVehicle();
-	int parkingNumber = -1;
-	boolean parkingSpotAvailable;
-	Connection con = null;
-	try {
-	    // check that ticket had been saved in DB
-	    con = dataBaseTestConfig.getConnection();
-	    PreparedStatement ps = con.prepareStatement("SELECT * FROM ticket WHERE VEHICLE_REG_NUMBER = ? ;");
-	    ps.setString(1, inputReaderUtil.readVehicleRegistrationNumber());
-	    ResultSet rs = ps.executeQuery();
-	    boolean hasTicket = rs.next();
-	    if (hasTicket) {
-		// THEN
-		assertThat(rs.getTimestamp("IN_TIME")).isNotNull();
-		assertThat(rs.getString("VEHICLE_REG_NUMBER"))
-			.isEqualTo(inputReaderUtil.readVehicleRegistrationNumber());
-		System.out.println("ticket was correctly saved in DB \n");
-	    }
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    System.out.println("Failed to check that ticket had been saved in DB \n");
-	} finally {
-	    dataBaseTestConfig.closeConnection(con);
-	}
-
-	// check that Parking table is updated with availability
-	try {
-	    // GIVEN
-	    // retreival of parking number linked to vehicle reg number.
-	    con = dataBaseTestConfig.getConnection();
-	    PreparedStatement ps2 = con
-		    .prepareStatement("SELECT PARKING_NUMBER FROM ticket WHERE VEHICLE_REG_NUMBER = ? ;");
-	    ps2.setString(1, inputReaderUtil.readVehicleRegistrationNumber());
-	    ResultSet rs2 = ps2.executeQuery();
-	    if (rs2.next()) {
-		parkingNumber = rs2.getInt("PARKING_NUMBER");
-	    }
-
-	    // WHEN
-	    // check parking to confirme that parkingspot is now unavailable
-	    PreparedStatement ps3 = con.prepareStatement("SELECT AVAILABLE FROM parking WHERE PARKING_NUMBER = ? ;");
-	    ps3.setInt(1, parkingNumber);
-	    ResultSet rs3 = ps3.executeQuery();
-	    boolean hasParking = rs3.next();
-	    if (hasParking) {
-		parkingSpotAvailable = rs3.getBoolean("AVAILABLE");
-
-		// THEN
-		assertThat(parkingSpotAvailable).isFalse();
-	    }
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    System.out.println("Failed to check if parking table as updated");
-	} finally {
-	    dataBaseTestConfig.closeConnection(con);
-	}
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        ParkingSpot parkingSpot = ticket.getParkingSpot();    
+        //THEN
+        assertThat(ticket.getVehicleRegNumber()).isEqualTo("ABCDEF");        
+        assertThat(parkingSpot.isAvailable()).isFalse();
+        
     }
 
     @Test
-    @DisplayName("test that the fare generated and out-time are populated correctly in the database when prow")
+    @DisplayName("test that the fare generated and out time are populated correctly in the database")
     public void testParkingLotExit() throws Exception {
-	// GIVEN
-	ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-
-	// WHEN
-	testParkingACar();
-	Thread.sleep(1000);
-	parkingService.processExitingVehicle();
-	Connection con = null;
-	try {
-	    con = dataBaseTestConfig.getConnection();
-	    PreparedStatement ps = con
-		    .prepareStatement("SELECT OUT_TIME, PRICE FROM ticket WHERE VEHICLE_REG_NUMBER = ? ;");
-	    ps.setString(1, inputReaderUtil.readVehicleRegistrationNumber());
-	    ResultSet rs = ps.executeQuery();
-	    boolean hasTicketWithOutTimeAndPrice;
-	    hasTicketWithOutTimeAndPrice = rs.next();
-	    if (hasTicketWithOutTimeAndPrice) {
-
-		// THEN
-		assertThat(rs.getDate("OUT_TIME")).isNotNull();
-		assertThat(rs.getDouble("PRICE")).isGreaterThanOrEqualTo(0);
-	    }
-	    System.out.println("fare generated and out time populated correctly");
-	} catch (Exception e) {
-	    fail("Failed to check if outtime and price populated in database" + e.getMessage());
-	} finally {
-	    dataBaseTestConfig.closeConnection(con);
-	}
+	//GIVEN
+    	testParkingACar();
+    	ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        Thread.sleep(500);//to pause between processing vehicle entrance and exit, to not creat issue with in-time and out-time being the same
+        //WHEN
+        parkingService.processExitingVehicle();
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");     
+        //THEN
+        assertThat(ticket.getPrice()).isEqualTo(0);
+        assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).isEqualTo(1);
+        assertThat(ticket.getOutTime()).isNotNull();
+    
     }
 
 }
